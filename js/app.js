c5ea0860029012d2af10afdc4402d7333b602b3f
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2022-05-24 13:40:32 +0200

    aggiunta snack 1

--- js/app.js
+++ js/app.js
@@ -2,9 +2,21 @@
 Inserisci un numero, se è pari stampa il numero, 
 se è dispari stampa il numero successivo.*/
 
+const inputNumber = parseInt(prompt("Inserisci un numero"))
+let numberStamp = document.getElementById("numberStamp")
+let finalNumber;
 
+if (inputNumber % 2 === 0) {
+    
+    finalNumber = inputNumber;
+    console.log(inputNumber);
+}
+if(inputNumber % 2 === 1) {
+    finalNumber = inputNumber + 1;
+    console.log(finalNumber);
+}
 
-
+numberStamp.innerHTML = `${finalNumber}`
 
 /*Snack2
 Generatore di “nomi cognomi” casuali: il Grande Gatsby 
