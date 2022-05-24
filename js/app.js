27ca83911b31f580973d7698b706a31d831baaa7
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2022-05-24 13:27:29 +0200

    preparazione esercizio

---
+++ js/app.js
@@ -0,0 +1,21 @@
+/*Snack1
+Inserisci un numero, se è pari stampa il numero, 
+se è dispari stampa il numero successivo.*/
+
+
+
+
+
+/*Snack2
+Generatore di “nomi cognomi” casuali: il Grande Gatsby 
+ha  una lista di nomi e una lista di cognomi, e da queste
+vuole generare una falsa lista di invitati con nome e cognome.*/
+
+
+
+
+
+
+/*Snack3
+Crea un array di numeri interi e fai la somma di tutti 
+gli elementi  che sono in posizione (indice) dispari*/
