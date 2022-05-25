737243044537121f83229b3d71d2c862a8a932e1
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2022-05-25 10:10:28 +0200

    modifica esercizi 3

--- js/app.js
+++ js/app.js
@@ -66,3 +66,10 @@ for(let i=0; i<listaNumeri.length; i++) {
 
 console.log(somma);
 console.log(sommaIndiciDispari);
+let title=document.getElementById("title")
+title.innerHTML=`Somma numeri dispari: ${somma}`
+
+/*Snack4 (Bonus)
+Crea due array che hanno un numero di elementi diversi.  Aggiungi 
+elementi all’array che ha meno elementi fino a quando ne avrà 
+tanti quanti l’altro.*/
